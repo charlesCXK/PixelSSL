a03addb70ad2785658af0300162f05c36cd23e94
charlesCXK <715811763@qq.com>
2020-09-13 18:43:10 +0800

    update

--- task/sseg/aml/command.sh
+++ task/sseg/aml/command.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python3 run_cxk.py --cfg deeplabv2_pascalvoc_full_suponly.sh
+#python3 run_cxk.py --cfg deeplabv2_pascalvoc_full_suponly.sh
 python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_suponly.sh
 python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_suponly_cxk.sh
 python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_sslgct.sh
