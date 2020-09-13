84360f1f7d231ebb49eb11db4de42715b9efd456
charlesCXK <715811763@qq.com>
2020-09-13 13:27:06 +0800

    update

--- task/sseg/aml/command.sh
+++ task/sseg/aml/command.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+python3 run_cxk.py --cfg deeplabv2_pascalvoc_full_suponly.sh
+python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_suponly.sh
 python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_suponly_cxk.sh
-python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_sslgct_cxk.sh
+python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_sslgct.sh
+python3 run_cxk.py --cfg deeplabv2_pascalvoc_1-8_sslgct.sh
--- task/sseg/aml_script/deeplabv2_pascalvoc_1-8_sslgct.sh
+++ task/sseg/aml_script/deeplabv2_pascalvoc_1-8_sslgct.sh
@@ -2,5 +2,5 @@
 
 source activate ssc
 export NGPUS=8
-CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 python -m script.deeplabv2_pascalvoc_1-8_sslgct
