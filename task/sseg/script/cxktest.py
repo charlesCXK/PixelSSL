4edad4d3468c30c17ad13b82404e9628ab0e1529
charlesCXK <715811763@qq.com>
2020-09-15 16:41:01 +0800

    update

--- task/sseg/script/cxktest.py
+++ task/sseg/script/cxktest.py
@@ -4,28 +4,40 @@ import collections
 
 try:
     import pixelssl
+
     pixelssl.log_info('Use installed pixelssl=={0}\n'.format(pixelssl.__version__))
 except ImportError:
     sys.path.append('../..')
     import pixelssl
+
     pixelssl.log_warn('No installed pixelssl, the latest code of PixelSSL will be used.\n')
 
 import proxy
 
-
 config = collections.OrderedDict(
     [
-        ('exp_id', os.path.basename(__file__).split(".")[0]),
-        
         # arguments - SSL algorithm
-        ('ssl_algorithm', pixelssl.SSL_NULL),
+        ('exp_id', os.path.basename(__file__).split(".")[0]),
+        ('ssl_algorithm', pixelssl.SSL_GCT),
+
+        ('ssl_mode', 'gct'),
+        ('fc_ssl_scale', 1.0),
+        ('dc_ssl_scale', 100.0),
+        ('dc_threshold', 0.6),
+        ('dc_rampup_epochs', 3),
+
+        ('fd_lr', 0.0001),
+        ('fd_scale', 10.0),
+
+        ('mu', 0.5),
+        ('nu', 1),
 
         # arguments - exp
-        ('resume', 'result/deeplabv2_pascalvoc_full_suponly/2020-09-12_02:26:18/ckpt/checkpoint_40.ckpt'),
+        ('resume', 'result/deeplabv2_pascalvoc_1-8_sslgct_cxk/2020-09-14_13:39:39/ckpt/checkpoint_20.ckpt'),
         ('validation', True),
-        
+
         ('out_path', 'result'),
-        
+
         ('visualize', False),
         ('debug', False),
 
@@ -40,12 +52,15 @@ config = collections.OrderedDict(
         ('num_workers', 2),
         ('im_size', 321),
 
+        ('sublabeled_path', 'dataset/PascalVOC/sublabeled_prefix/1-8/train_aug_labeled_1-8.txt'),
+        ('ignore_unlabeled', False),
+
         # arguments - task specific components
         ('models', {'model': 'deeplabv2'}),
         ('optimizers', {'model': 'sgd'}),
         ('lrers', {'model': 'polynomiallr'}),
         ('criterions', {'model': 'deeplab_criterion'}),
-        
+
         # arguments - task specific optimizer / lr scheduler
         ('lr', 0.00025),
         ('momentum', 0.9),
@@ -56,13 +71,12 @@ config = collections.OrderedDict(
         ('backbone', 'resnet'),
 
         # arguments - training details
-        ('epochs', 40),
-        ('batch_size', 1),
-        ('unlabeled_batch_size', 0), 
+        ('epochs', 20),
+        ('batch_size', 4),
+        ('unlabeled_batch_size', 2),
 
     ]
 )
-
 
 if __name__ == '__main__':
     pixelssl.run_script(config, proxy, proxy.SemanticSegmentationProxy)
